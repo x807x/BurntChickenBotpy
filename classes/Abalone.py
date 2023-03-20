6c28f52506d664bbebe565797f22dfd1650c1f43
TaiwanRzara <86676478+TaiwanRzara@users.noreply.github.com>
2023-03-20 09:16:10 +0800

    Update Abalone.py

--- classes/Abalone.py
+++ classes/Abalone.py
@@ -1,7 +1,7 @@
 #include<bits/stdc++.h>
 #define int long long
 #define ios ios_base::sync_with_stdio(false),cin.tie(0),cout.tie(0);
-
+#define ull unsigned long long
 import math, copy
 graph=('⭕  ','⚽  ','🥎  ','  ')
 
