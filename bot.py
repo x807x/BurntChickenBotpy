614b1c65889726eecbf260582d5597f11e13a3da
TaiwanRzara <86676478+TaiwanRzara@users.noreply.github.com>
2023-03-21 00:22:49 +0800

    2023/03/21

--- bot.py
+++ bot.py
@@ -12,17 +12,22 @@ with open('./data/setting.json','r',encoding='utf8') as file2:
     SettingData=json.load(file2)
 
 bot = commands.Bot(command_prefix= SettingData['prefix'], owner_ids= data['Owner'],intents=intents)
+async def Cog_load():
+	for filename in os.listdir('./cmds'):
+		if filename.endswith('.py'):
+			print(f"Cog_load{filename[:-3]}")
+			await bot.load_extension(f'cmds.{filename[:-3]}')
+	for filename in os.listdir("./txts"):
+		if(filename.endswith('.py')):
+			print(f"Cog_load{filename[:-3]}")
+			await bot.load_extension(f"txts.{filename[:-3]}")
 
 @bot.event
 async def on_ready():
-	print(f">> {bot.user.name} is online <<")
+    print(f">> {bot.user.name} is online <<")
+    
 
-for filename in os.listdir('./cmds'):
-	if filename.endswith('.py'):
-		bot.load_extension(f'cmds.{filename[:-3]}')
-for filename in os.listdir("./txts"):
-    if(filename.endswith('.py')):
-        bot.load_extension(f"txts.{filename[:-3]}")
-
-if __name__ == "__main__":
-	bot.run(data['Token'])
+async def main():
+    await Cog_load()
+    await bot.start(data['TestToken'])
+asyncio.run(main())
--- cmds/main.py
+++ cmds/main.py
@@ -1,13 +1,20 @@
 import discord
 from discord import app_commands
 from discord.ext import commands
+
 class Pong(commands.Cog):
-    def __init__(self,bot:commands.Bot):
+    def __init__(self,bot):
         self.bot=bot
+        super().__init__()
     @commands.Cog.listener()
     async def on_ready(self):
         print("Ping")
-    
-    @app_commands.command(name="pong",description="Get Bot's ping")
+    @app_commands.command(name="ping",description="Get Bot's ping")
     async def ping(self,interaction:discord.Interaction):
-        await interaction.response.send_message(f"Ping")
+        await interaction.response.send_message(f"Ping",ephemeral=True)
+        return
+
+
+async def setup(bot):
+    await bot.add_cog(Pong(bot),guilds=[discord.Object(id=1065833518871085107)])
+    print("Main Setup")
