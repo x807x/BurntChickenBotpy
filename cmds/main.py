5846eeeae239b39c625b5b9baba8ed51f838180e
TaiwanRzara <86676478+TaiwanRzara@users.noreply.github.com>
2023-03-26 23:35:35 +0800

    add worship command

--- cmds/main.py
+++ cmds/main.py
@@ -1,20 +1,36 @@
+# -*- coding:utf-8 -*-
 import discord
-from discord import app_commands
 from discord.ext import commands
-
+pray = """
+**      **      🛐🛐
+      🛐            🛐
+🛐                        🛐
+            🛐🛐
+            🛐🛐
+      🛐   🛐
+         🛐🛐
+               🛐
+            🛐🛐🛐
+"""
 class Pong(commands.Cog):
     def __init__(self,bot):
         self.bot=bot
         super().__init__()
     @commands.Cog.listener()
     async def on_ready(self):
-        print("Ping")
-    @app_commands.command(name="ping",description="Get Bot's ping")
-    async def ping(self,interaction:discord.Interaction):
-        await interaction.response.send_message(f"Ping",ephemeral=True)
-        return
+        print("Pong Loaded")
+    @commands.command()
+    async def ping(self,ctx):
+        await ctx.send(f"bot.latency= {round(self.bot.latency*1000)}ms")
+        return 
+    @commands.command(name="🛐",pass_context=True)
+    async def place_of_worship(self,ctx):
+        await ctx.send(pray)
+        return 
+    commands.command(name="worship",pass_context=True)(place_of_worship.callback)
+    commands.command(name="電神",pass_context=True)(place_of_worship.callback)
 
 
 async def setup(bot):
-    await bot.add_cog(Pong(bot),guilds=[discord.Object(id=1065833518871085107)])
+    await bot.add_cog(Pong(bot))
     print("Main Setup")
