0b710b4cb51bd238271d5adace0e67b92b6011fc
TaiwanRzara <86676478+TaiwanRzara@users.noreply.github.com>
2023-03-29 00:23:18 +0800

    tomerge

--- cmds/main.py
+++ cmds/main.py
@@ -20,26 +20,19 @@ class Pong(commands.Cog):
     @commands.Cog.listener()
     async def on_ready(self):
         print("Pong Loaded")
-    @commands.command()
+    @commands.hybrid_command(name="ping",pass_context=True,discription="Send My Ping")
     async def ping(self,ctx):
         await ctx.send(f"bot.latency= {round(self.bot.latency*1000)}ms")
         return 
-    @commands.command(name="🛐",pass_context=True)
+    @commands.hybrid_command(name="膜拜",pass_context=True)
     async def place_of_worship(self,ctx):
         with open(file="./data/cmd_useable.json",mode="r",encoding="utf-8") as permission_json:
             permission=json.load(permission_json)
-        print(ctx.channel.id)
         if(str(ctx.channel.id) in permission["place_of_worship"]["unable"]):
             await ctx.reply("You can't use this command in this guild")
             return
-        await ctx.send(pray)
+        await ctx.reply(pray)
         return
-    @commands.command(name="worship",pass_context=True)
-    async def place_of_worship2(self,ctx):
-        print("worship")
-        print(ctx.guild.id)
-        await self.place_of_worship.callback
-        print("hi")
     @commands.hybrid_command(name="hi",with_app_command=True,guild=discord.Object(id=1048972316924711003),description="say hello")
     async def ping_command(self, ctx: commands.Context) -> None:
         await ctx.defer()
