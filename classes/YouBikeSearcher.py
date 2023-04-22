6fe4d9ff46997c55bfe40c7f407a4c37e85ae977
TaiwanRzara <86676478+TaiwanRzara@users.noreply.github.com>
2023-04-23 00:42:04 +0800

    import search youbike

--- classes/YouBikeSearcher.py
+++ classes/YouBikeSearcher.py
@@ -1,7 +1,9 @@
 from classes.TDXGetter import TDXGetter
 import json
+import time
 DataGetter=TDXGetter()
-
+url_station="https://tdx.transportdata.tw/api/basic/v2/Bike/Station/City/"
+url_av="https://tdx.transportdata.tw/api/basic/v2/Bike/Availability/City/"
 table="""
 剩餘YouBike
   | 剩餘空車架
@@ -31,20 +33,40 @@ class YouBikeStation:
         self.ERROR=True
         return None
 
+    def clear(self): return self.__init__()
+
     def __repr__(self)->str:
         return f"{its(self.rentable)} {its(self.returnable)} {self.version} {self.name}"
 
     def ToStringEn(self)->str:
         return f"{its(self.rentable)} {its(self.returnable)} {self.version} {self.name_en}"
 
-    async def Update(self)->str:
-        url=f"https://tdx.transportdata.tw/api/basic/v2/Bike/Availability/City/{self.city}?%24filter=contains%28StationID%2C%27{self.id}%27%29&%24orderby=StationID&%24top=20&%24format=JSON"
-        data=json.loads(DataGetter.get_data(url))
+    def station_loads(self,city:str,data:dict):
+        self.version=data["ServiceType"]
+        self.id=data["StationID"]
+        temp=data["StationName"]["Zh_tw"]
+        if(temp.startswith(f"YouBike{self.version}.0_") and temp[10]=='_'): temp=temp[11:]
+        self.name=temp
+        temp=data["StationName"]["En"]
+        if(temp.startswith(f"YouBike{self.version}.0_") and temp[10]=='_'): temp=temp[11:]
+        self.name_en=temp
+        self.capacity=data["BikesCapacity"]
+        self.longitude=data["StationPosition"]["PositionLon"]
+        self.latitude=data["StationPosition"]["PositionLat"]
+        self.city=city
+        return self
+
+    def av_loads(self,data:dict):
         self.rentable=data["AvailableRentBikes"]
         self.returnable=data["AvailableReturnBikes"]
-        self.rentable_general=data["AvailableRentBikesDetail"]["General"]
+        self.rentable_general=data["AvailableRentBikesDetail"]["GeneralBikes"]
         self.rentable_electric=data["AvailableRentBikesDetail"]["ElectricBikes"]
+        return self
 
+    async def Update(self)->str:
+        url=f"{url_av}{self.city}?$filter=StationID eq '{self.id}'&$top=1&$format=JSON"
+        data=json.loads(DataGetter.get_data(url))
+        self.av_loads(data)
         return self
 
     async def find(self,city:str,id:int):
@@ -62,54 +84,52 @@ class YouBikeStation:
         self.latitude=0
         self.ERROR=True
         if(city=="" or id==0): return None
-        url=f"https://tdx.transportdata.tw/api/basic/v2/Bike/Station/City/{city}?%24filter=contains%28StationID%2C%27{id}%27%29&%24orderby=StationID&%24top=30&%24format=JSON"
+        url=f"{url_station}{city}?$filter=StationID eq '{id}'&$top=1&$format=JSON"
         data=DataGetter.get_data(url)
         data=json.loads(data)
-        if(len(data)>1):
-            l=0;r=len(data)
-            if(int(data[0]["StationID"])<id):
-                data=[]
-            
-        if(len(data)==1):
-            data=data[0]
-            self.version=data["ServiceType"]
-            self.id=data["StationID"]
-            temp=data["StationName"]["Zh_tw"]
-            if(temp.startswith(f"YouBike{self.version}.0_") and temp[10]=='_'): temp=temp[11:]
-            self.name=temp
-            temp=data["StationName"]["En"]
-            if(temp.startswith(f"YouBike{self.version}.0_") and temp[10]=='_'): temp=temp[11:]
-            self.name_en=temp
-            self.capacity=data["BikesCapacity"]
-            self.longitude=data["StationPosition"]["PositionLon"]
-            self.latitude=data["StationPosition"]["PositionLat"]
+        self.station_loads(city,data[0])
 
-        url=f"https://tdx.transportdata.tw/api/basic/v2/Bike/Availability/City/{city}?%24filter=contains%28StationID%2C%27{id}%27%29&%24orderby=StationID&%24top=20&%24format=JSON"
+        url=f"{url_av}{city}?$filter=StationID eq '{self.id}'&$top=1&$format=JSON"
         data=json.loads(DataGetter.get_data(url))
-        data=data[0]
-        self.city=city
-        self.id=data["StationID"]
-        self.rentable=data["AvailableRentBikes"]
-        self.returnable=data["AvailableReturnBikes"]
-        self.rentable_general=data["AvailableRentBikesDetail"]["GeneralBikes"]
-        self.rentable_electric=data["AvailableRentBikesDetail"]["ElectricBikes"]
-        
+        self.av_loads(data[0])
+        self.ERROR=False
         return self
 
 
 class YouBikeSearcher:
+    async def updates(self,city:str,data:list):
+        this=YouBikeStation()
+        this.station_loads(city,data[0])
+        
+        stations=[]
+        stations.append(this)
+        url=f"{url_av}{city}?$filter=StationID eq '{this.id}'"
+        
+        for i in range(1,len(data)):
+            this=YouBikeStation()
+            this.station_loads(city,data[i])
+            stations.append(this)
+            url+=f" or StationID eq '{this.id}'"
+        url+=f"&$top={len(data)}&$format=JSON"
+        start=time.time()
+        data=json.loads(DataGetter.get_data(url))
+        for i in range(len(data)):
+            stations[i].av_loads(data[i])
+        return stations
+    
     async def name_get(self,city:str,name:str):
+        start=time.time()
         try:
-            url=f"https://tdx.transportdata.tw/api/basic/v2/Bike/Station/City/{city}?%24select=StationID&%24filter=contains%28StationName%2FZh_tw%2C%27{name}%27%29&%24top=10000&%24format=JSON"
+            url=f"{url_station}{city}?$filter=contains(StationName/Zh_tw,'{name}')&$top=10000&$format=JSON"
             data=json.loads(DataGetter.get_data(url))
         except:
             print("ERROR")
-        if(len(data)>30):
-            return f"找到{len(data)}個站點\n需要更詳細的名稱"
-        string="```py\n"+table
-        temp=YouBikeStation()
+        if(len(data)==0):
+            return "找到不到任何站點"
+        
+        data=await self.updates(city,data)
+        string="```py"+table
         for station in data:
-            await temp.find(city,station["StationID"])
-            string+=f"{temp}\n"
-        string+=f"```\n找到{len(data)}個站點"
+            string+=f"{station}\n"
+        string+=f"```找到{len(data)}個站點  搜尋時間{time.time()-start}"
         return string
