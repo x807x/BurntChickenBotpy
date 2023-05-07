ecd025078b2a3765a02df4c667f98029de2ff847
TaiwanRzara <86676478+TaiwanRzara@users.noreply.github.com>
2023-05-07 21:58:17 +0800

    fix Daily bug

--- TokenSetter.py
+++ TokenSetter.py
@@ -1,6 +1,6 @@
 import os
-while True:
-    command=input("please input {key_name},{key_value}\n")
-    if(command=="exit" or command=="exit()"): break
-    key=command.split(",")
-    os.environ[key[0]]=key[1]
+os.environ["Owner"]="872315702860718080"
+os.environ["Token"]="MTA1MTA2NTE0MzE3OTYwODA4NA.GojY3d.HFNKQl2NbrGAWuY_7_ah4_82wfj9SzRagGtuss"
+os.environ["TestToken"]="MTA4NzM3MzMyMjI5NzI3ODUyNA.GwJXZI.uV-gnPJLuy5yv1fOTwnfqybSZOUIjyNwtK5--0"
+os.environ["TDXID"]="ck1110807-183fb5aa-c32d-4fb0"
+os.environ["TDXKey"]="c3f0591f-c0bd-4a61-bb32-e153520abb9d"
